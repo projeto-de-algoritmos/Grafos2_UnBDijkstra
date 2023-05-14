ae9ba8538254ebdc330247206dd09a0231901691
Antonio Aldisio <aldisiofilho@gmail.com>
2023-05-14 09:35:12 -0300

    ajuste
    co-authored-by: Rodrigo Balbino <rodrigo080599@hotmail.com>

--- teste.py
+++ teste.py
@@ -3,7 +3,6 @@ from grafos import Grafo
 
 grafo = Grafo()
 
-# Adicionando os prédios
 grafo.adicionar_predio("Prédio da Reitoria")
 grafo.adicionar_predio("Biblioteca Central")
 grafo.adicionar_predio("ICC")
@@ -12,18 +11,13 @@ grafo.adicionar_predio("FE")
 grafo.adicionar_predio("FS")
 grafo.adicionar_predio("FT")
 
-# Adicionando as conexões com peso
 grafo.adicionar_conexao("ICC", "FT", 3)
 grafo.adicionar_conexao("ICC", "FE", 2)
 grafo.adicionar_conexao("Biblioteca Central", "ICC", 1)
 
-# Executando o algoritmo de Dijkstra a partir do prédio inicial
-# Definindo o prédio de origem e o prédio de destino
 predio_origem = "Biblioteca Central"
 predio_destino = "FE"
 
-# Executando o algoritmo de Dijkstra para encontrar a menor distância entre os prédios
 distancia = grafo.dijkstra(predio_origem, predio_destino)
 
-# Imprimindo as distâncias mínimas a partir do prédio inicial
 print("Menor distância entre", predio_origem, "e", predio_destino + ":", distancia)

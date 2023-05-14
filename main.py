e1905b22ade72cedc0baf9766b07c7ffecc6ea32
Rodrigo Balbino <rodrigo080599@hotmail.com>
2023-05-14 10:58:54 -0300

    add character and movement
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from pathfinding.finder.a_star import AStarFinder
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.finder.dijkstra import DijkstraFinder
 
-
 class Pathfinder:
     def __init__(self, matrix):
         """
@@ -13,6 +12,12 @@ class Pathfinder:
         self.matrix = matrix
         self.grid = Grid(matrix=matrix)
         self.select_surf = pygame.image.load('selection.png').convert_alpha()
+        #path finding
+        self.path = []
+        #character
+        self.character = pygame.sprite.GroupSingle(Character(self.empty_path))
+
+    def empty_path(self):
         self.path = []
 
     def draw_active_cell(self):
@@ -31,17 +36,23 @@ class Pathfinder:
         """
         start
         """
-        start_x, start_y = [1, 1]
+        start_x, start_y = self.character.sprite.get_coord()
         start = self.grid.node(start_x, start_y)
+        """
+        end
+        """
         mouse_pos = pygame.mouse.get_pos()
         end_x, end_y = mouse_pos[0] // 32, mouse_pos[1] // 32
         end = self.grid.node(end_x, end_y)
 
-        # finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
+        """
+        path
+        """
         finder = DijkstraFinder(diagonal_movement=DiagonalMovement.always)
         self.path, _ = finder.find_path(start, end, self.grid)
         self.grid.cleanup()
-        print(self.path)
+        #print(self.path)
+        self.character.sprite.set_path(self.path)
 
     def draw_path(self):
         if self.path:
@@ -56,6 +67,70 @@ class Pathfinder:
     def update(self):
         self.draw_active_cell()
         self.draw_path()
+
+        #character updating and drawing
+        self.character.update()
+        self.character.draw(screen)
+
+
+class Character(pygame.sprite.Sprite):
+    def __init__(self, empty_path):
+        # basic
+        super().__init__()
+        self.image = pygame.image.load('R&A.png').convert_alpha()
+        self.rect = self.image.get_rect(center = (60,60))
+
+        #movement
+        self.pos = self.rect.center
+        self.speed = 2
+        self.direction = pygame.math.Vector2(0,0)
+
+        #path
+        self.path = []
+        self.collision_rects = []
+        self.empty_path = empty_path
+
+    def get_coord(self):
+        col = self.rect.centerx // 32
+        row = self.rect.centery // 32
+        return (col, row)
+
+    def set_path(self,path):
+        self.path = path
+        self.create_collision_rects()
+        self.get_direction()
+
+    def create_collision_rects(self):
+        if self.path:
+            self.collision_rects = []
+            for point in self.path:
+                x = (point[0] * 32) + 16
+                y = (point[1] * 32) + 16
+                rect = pygame.Rect((x - 2,y - 2),(4,4))
+                self.collision_rects.append(rect)
+
+    def get_direction(self):
+        if self.collision_rects:
+            start = pygame.math.Vector2(self.pos)
+            end = pygame.math.Vector2(self.collision_rects[0].center)
+            self.direction = (end - start).normalize()
+        else:
+            self.direction = pygame.math.Vector2(0,0)
+            self.path = []
+
+    def check_collisions(self):
+        if self.collision_rects:
+            for rect in self.collision_rects:
+                if rect.collidepoint(self.pos):
+                    del self.collision_rects[0]
+                    self.get_direction()
+        else:
+            self.empty_path()
+
+    def update(self):
+        self.pos += self.direction * self.speed
+        self.check_collisions()
+        self.rect.center = self.pos
 
 
 #pygame setup
